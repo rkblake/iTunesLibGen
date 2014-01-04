d49d18db0705383584faa7b4817ea93bb57f4690
Joshua <jdong42@gmail.com>
2014-01-03 20:54:14 -0600

    Added date header and changed code to fit better in 80 chars.

--- iTunesLibGen.py
+++ iTunesLibGen.py
@@ -3,7 +3,6 @@ import os
 import xml.etree.ElementTree as ET
 from mutagen.mp3 import MP3
 
-i = 0
 
 def add_key(dst, name, data_type, value):
     new = ET.SubElement(dst, "key")
@@ -53,7 +52,7 @@ def format_path(path):
         str = 'file://localhost' + str
     str = str.replace(' ','%20')
     return str
-                          
+
 def split_trackdisc(frame, first):
     index = str.find('/')
     if first:
@@ -67,28 +66,37 @@ def get_filesize(path):
     else:
         return os.path.getsize(path)
 
-path = sys.argv[1]
-library = open('iTunes Music Library.xml', 'w')
-library.write('<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n')
+if __name__ == "__main__":
+    i = 0
+    path = sys.argv[1]
+    library = open('iTunes Music Library.xml', 'w')
+    library.write('<?xml version="1.0" encoding="UTF-8"?>\n'
+                  '<!DOCTYPE plist PUBLIC '
+                  '"-//Apple Computer//DTD PLIST 1.0//EN" '
+                  '"http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n')
 
-plist = ET.Element('plist')
-plist.attrib = {'version': '1.0'}
-plist.text = '\n'
-mdict = ET.SubElement(plist, 'dict')
-mdict.text = '\n\t'
-add_key(mdict, "Major Version", "integer", 1)
-add_key(mdict, "Minor Version", "integer", 1)
-add_key(mdict, "Date", "date", "1994-12-18T00:00:00Z")                  #TODO: function for date in format: <YYYY-MM-DD>T<HH:MM:SS>Z
-add_key(mdict, "Application Version", "string", "11.1.3")               #TODO: function for itunes version
-add_key(mdict, "Features", "integer", 5)
-add_key(mdict, "Show Content Ratings", "true", None)
-add_key(mdict, "Music Folder", "path", format_path(path))
-add_key(mdict, "Library Persistent ID", "string", "0123456789abcdef")   #TODO: this?
-add_key(mdict, "Tracks", None, None)
-sdict = ET.SubElement(mdict, 'dict')
-sdict.text = '\n\t\t'
+    plist = ET.Element('plist')
+    plist.attrib = {'version': '1.0'}
+    plist.text = '\n'
+    mdict = ET.SubElement(plist, 'dict')
+    mdict.text = '\n\t'
+    add_key(mdict, "Major Version", "integer", 1)
+    add_key(mdict, "Minor Version", "integer", 1)
+    add_key(mdict, "Date", "date",
+            datetime(datetime.now().year,
+                     datetime.now().month,
+                     datetime.now().day,
+                     datetime.now().hour,
+                     datetime.now().minute,
+                     datetime.now().second).isoformat()+"Z")
+    add_key(mdict, "Application Version", "string", "11.1.3")               #TODO: function for itunes version
+    add_key(mdict, "Features", "integer", 5)
+    add_key(mdict, "Show Content Ratings", "true", None)
+    add_key(mdict, "Music Folder", "path", format_path(path))
+    add_key(mdict, "Library Persistent ID", "string", "0123456789abcdef")   #TODO: this?
+    add_key(mdict, "Tracks", None, None)
+    sdict = ET.SubElement(mdict, 'dict')
+    sdict.text = '\n\t\t'
 
-
-
-library.write(ET.tostring(plist))
-library.close()
+    library.write(ET.tostring(plist))
+    library.close()
